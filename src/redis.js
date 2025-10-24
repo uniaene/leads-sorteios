c6dd75b7ea04bf98a815ef2ac153c9d195ca11a1
Fernando Issler <fernandoissler@hotmail.com>
2025-10-24 02:32:36 -0300

    Erro field

--- src/redis.js
+++ src/redis.js
@@ -1,16 +1,22 @@
-import Redis from "ioredis";
-import dotenv from "dotenv";
-dotenv.config();
+import { createClient } from "redis";
 
-const redis = new Redis({
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: Number(process.env.REDIS_PORT) || 6379,
-    // password: process.env.REDIS_PASSWORD,
-    maxRetriesPerRequest: 2,
-    enableReadyCheck: true
+// Nome da fila
+export const LEADS_QUEUE = "leads_queue";
+
+// Cria cliente Redis
+const redis = createClient({
+    socket: {
+        host: process.env.REDIS_HOST || "127.0.0.1",
+        port: process.env.REDIS_PORT || 6379,
+    },
+    password: process.env.REDIS_PASSWORD || undefined,
 });
 
-// chave da fila
-export const LEADS_QUEUE = "leads_queue";
+redis.on("error", (err) => {
+    console.error("❌ Erro no Redis:", err.message);
+});
+
+await redis.connect();
+console.log("✅ Conectado ao Redis com sucesso");
 
 export default redis;
