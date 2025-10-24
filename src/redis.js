273fbf68703c3f692045b46e2947d297bd30a6fc
Fernando Issler <fernandoissler@hotmail.com>
2025-10-24 02:35:17 -0300

    Erro field

--- src/redis.js
+++ src/redis.js
@@ -1,9 +1,7 @@
 import { createClient } from "redis";
 
-// Nome da fila
 export const LEADS_QUEUE = "leads_queue";
 
-// Cria cliente Redis
 const redis = createClient({
     socket: {
         host: process.env.REDIS_HOST || "127.0.0.1",
@@ -12,10 +10,7 @@ const redis = createClient({
     password: process.env.REDIS_PASSWORD || undefined,
 });
 
-redis.on("error", (err) => {
-    console.error("❌ Erro no Redis:", err.message);
-});
-
+redis.on("error", (err) => console.error("❌ Erro no Redis:", err.message));
 await redis.connect();
 console.log("✅ Conectado ao Redis com sucesso");
 
